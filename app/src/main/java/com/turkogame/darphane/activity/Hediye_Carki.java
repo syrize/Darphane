6cad1cdbec8c008fa6fb053e2e3ee333c085f6c5
syrize <syrize@hotmail.com>
2022-07-18 01:54:25 +0300

    Bil kazan ekran çalışmasına devam edildi

--- app/src/main/java/com/turkogame/darphane/activity/Hediye_Carki.java
+++ app/src/main/java/com/turkogame/darphane/activity/Hediye_Carki.java
@@ -1,5 +1,7 @@
 package com.turkogame.darphane.activity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -7,11 +9,14 @@ import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,12 +24,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.beloo.widget.chipslayoutmanager.util.log.Log;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import com.turkogame.darphane.R;
+import com.turkogame.darphane.activity.app.AppConfig;
 import com.turkogame.darphane.utils.Tools;
 
 import java.util.Random;
 
-public class Hediye_Carki extends AppCompatActivity {
+public class Hediye_Carki extends AppCompatActivity implements RewardedVideoAdListener{
 
     private static final String [] sectors ={"20","1","10","30","5","50","20","1","10","30","5","50"};
     private static final int [] sectorDegrees = new int[sectors.length];
@@ -32,10 +43,17 @@ public class Hediye_Carki extends AppCompatActivity {
     private int degree= 0;
     private boolean isSpinning = false;
 
+
     private ImageView wheel;
-    SharedPreferences sharedPreferences,bakiye_kontrol,kayit_kontrol,kullanim_kontrol,kullanim_kayit;
+    SharedPreferences sharedPreferences,kayit_kontrol,bakiye_kontrol;
     String kullanici_id,paket_id,paket_adi,paket_turux,kredi_bedeli,token;
-    TextView kredi;
+    TextView kredi,cevirme_hakki;
+
+    private static final String APP_ID = AppConfig.APP_ID;
+    private static final String AD_UNIT_ID = AppConfig.odullu_reklam_id;
+
+    private RewardedVideoAd mRewardedVideoAd;
+    AdRequest adRequest;
 
 
 
@@ -59,11 +77,20 @@ public class Hediye_Carki extends AppCompatActivity {
         ImageView wheel = findViewById(R.id.wheel);
         float ilk_rota = wheel.getRotation();
         kredi = (TextView) findViewById(R.id.kredi);
+        cevirme_hakki = (TextView) findViewById(R.id.cevirme_hakki);
+
+        MobileAds.initialize(this, APP_ID);
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
+        mRewardedVideoAd.setUserId(kullanici_id);
+        adRequest = new AdRequest.Builder().build();
+        loadRewardedVideoAd();
 
         sharedPreferences = getApplicationContext().getSharedPreferences("giris", 0);
         kullanici_id = sharedPreferences.getString("user_id","0");
 
         Kredi_Girisi.kredi_oku(kullanici_id,"2" );
+
 
         bakiye_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
 
@@ -71,55 +98,80 @@ public class Hediye_Carki extends AppCompatActivity {
 
 
 
+
+        kayit_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
+
+            if (kayit_kontrol.getString("hak","0") != null &&
+                    kayit_kontrol.getString("hak","0")!="") {
+
+                cevirme_hakki.setText(kayit_kontrol.getString("hak", "0"));
+            }
+
+
+
         spinBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                getDegreeForSectors();
 
-                wheel.setRotation(ilk_rota);
+                if (Integer.parseInt(cevirme_hakki.getText().toString())>0 ) {
 
+                    getDegreeForSectors();
 
-                degree = random.nextInt(sectors.length-1);
+                    wheel.setRotation(ilk_rota);
 
-                wheel.animate().rotation(360*sectors.length+sectorDegrees[degree]).setDuration(360*sectors.length).start();
+                    degree = random.nextInt(sectors.length - 1);
 
+                    wheel.animate().rotation(360 * sectors.length + sectorDegrees[degree]).setDuration(360 * sectors.length).start();
 
-                new CountDownTimer(5000, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-
-                    }
-
-                    @Override
-                    public void onFinish() {
-
-                        if ( wheel.getRotation()==360*sectors.length+sectorDegrees[degree]) {
-
-                            Kredi_Girisi.kredi_satinalma(kullanici_id,"9" ,"6", sectors[degree],"0","3");
-
-
-
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    bakiye_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
-
-                                    kredi.setText(bakiye_kontrol.getString("kredi","0"));
-
-
-                                }
-                            }, 1000);
-
-
+                    new CountDownTimer(5000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
 
                         }
 
-                    }
-                }.start();
+                        @Override
+                        public void onFinish() {
+
+                            if (wheel.getRotation() == 360 * sectors.length + sectorDegrees[degree]) {
+
+                                Kredi_Girisi.kredi_satinalma(kullanici_id, "9", "6", sectors[degree], "0", "3");
 
 
+                                int hak=0;
+                                kayit_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
 
+                                hak = Integer.parseInt(kayit_kontrol.getString("hak", "0"));
+                                hak = hak - 1;
+
+                                SharedPreferences.Editor kayitci = kayit_kontrol.edit();
+                                kayitci.putString("hak" , String.valueOf(hak));
+                                kayitci.commit();
+                                cevirme_hakki.setText(String.valueOf(hak));
+
+
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        bakiye_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
+
+                                        kredi.setText(bakiye_kontrol.getString("kredi", "0"));
+
+
+                                    }
+                                }, 2000);
+
+
+                            }
+
+                        }
+                    }.start();
+
+
+                } else {
+                    cevirme_hakki_kazan();
+
+                }
 
 
             }
@@ -166,6 +218,152 @@ public class Hediye_Carki extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void cevirme_hakki_kazan() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.cevirmehakkikazan);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.verticalMargin=50;
+
+        final LinearLayout reklam_izle = (LinearLayout) dialog.findViewById(R.id.reklam_izle);
+        final Button yeni_oyun = (Button) dialog.findViewById(R.id.yeni_oyun);
+        final TextView reklam_izle_kredisi = (TextView) dialog.findViewById(R.id.reklam_izle_kredisi);
+        final TextView oyunsonu = (TextView) dialog.findViewById(R.id.oyunsonu);
+        final ImageView home = (ImageView) dialog.findViewById(R.id.home);
+
+        ((ImageView) dialog.findViewById(R.id.home)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+
+            }
+        });
+
+        ((Button) dialog.findViewById(R.id.yeni_oyun)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.cancel();
+            }
+        });
+
+        ((LinearLayout) dialog.findViewById(R.id.reklam_izle)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRewardedVideoAd.show();
+            }
+        });
+
+
+
+
+        dialog.setCancelable(false);
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
+
+    //Video Reklam yükle
+    private void loadRewardedVideoAd() {
+        if (!mRewardedVideoAd.isLoaded()) {
+
+            mRewardedVideoAd.loadAd(AD_UNIT_ID, adRequest);
+
+        }
+    }
+
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        // Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "Ödüllü Video Reklam Yüklendi", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+       // Toast.makeText(this, "Ödüllü Video Reklam Açıldı", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+       // Toast.makeText(this, "Ödüllü Video Başladı", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+       // Toast.makeText(this, "Ödüllü Video Reklamında Kapatıldı", Toast.LENGTH_SHORT).show();
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+        String miktar = String.valueOf(rewardItem.getAmount());
+
+        int hak=0;
+        kayit_kontrol = getApplicationContext().getSharedPreferences("darphane_kontrol", 0);
+
+        if (kayit_kontrol.getString("hak","0") != null && kayit_kontrol.getString("hak","0")!="") {
+            hak = Integer.parseInt(kayit_kontrol.getString("hak", "0"));
+            hak = hak + 3;
+        }else{
+            hak=3;
+        }
+
+        SharedPreferences.Editor kayitci = kayit_kontrol.edit();
+        kayitci.putString("hak" , String.valueOf(hak));
+        kayitci.commit();
+
+        cevirme_hakki.setText(String.valueOf(hak));
+
+
+        loadRewardedVideoAd();
+        //Toast.makeText(this, "onRewarded: "+rewardItem.getAmount(), Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+       // Toast.makeText(this, "onRewardedVideoAdLeftApplication", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+       // Toast.makeText(this, "Ödüllü Video Reklam Yüklenemedi", Toast.LENGTH_SHORT).show();
+          loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onRewardedVideoCompleted() {
+       // Toast.makeText(this, "Ödüllü Video Tamamlandı", Toast.LENGTH_SHORT).show();
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mRewardedVideoAd.resume(this);
+    }
+
+    @Override
+    public void onPause() {
+        mRewardedVideoAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mRewardedVideoAd.destroy(this);
+        super.onDestroy();
+    }
+
 
 
 }
